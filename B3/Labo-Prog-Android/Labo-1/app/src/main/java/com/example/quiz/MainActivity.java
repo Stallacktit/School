ab893af0324877a4fd020e5211a073159918a3ba
Stallacktit <47444702+Stallacktit@users.noreply.github.com>
2020-09-27 17:10:44 +0200

    update 1

--- B3/Labo-Prog-Android/Labo-1/app/src/main/java/com/example/quiz/MainActivity.java
+++ B3/Labo-Prog-Android/Labo-1/app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
