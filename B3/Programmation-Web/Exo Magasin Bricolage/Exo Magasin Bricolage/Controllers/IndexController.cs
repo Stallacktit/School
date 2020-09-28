0fc61b5030086e56b272a56374ba61deed6a2821
Stallacktit <47444702+Stallacktit@users.noreply.github.com>
2020-09-28 15:48:19 +0200

    School Works

---
+++ B3/Programmation-Web/Exo Magasin Bricolage/Exo Magasin Bricolage/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exo_Magasin_Bricolage.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
