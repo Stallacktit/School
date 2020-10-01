b8e832ce58087f3621b4f8d8f56c46c5a6dcc7a8
Stallacktit <47444702+Stallacktit@users.noreply.github.com>
2020-10-01 08:56:23 +0200

    Added a login app in wpf

---
+++ B3/WPF/GestionEvent/GestionEvent/GestionEvent/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionEvent
+{
+    class Client
+    {
+        public Client(string nom, string prenom, string rue, string localite, string codePostale)
+        {
+            _Nom = nom;
+            _Prenom = prenom;
+            _Rue = rue;
+            _Localite = localite;
+            _CodePostale = codePostale;
+        }
+
+        string _Nom { get; set; }
+        string _Prenom{ get; set;    }
+        string _Rue{ get; set; }
+        string _Localite { get; set; }
+        string _CodePostale { get; set; }
+
+        public string GetName()
+        {
+            return _Nom;
+        }
+        public string GetFirstName()
+        {
+            return _Prenom;
+        }
+
+
+
+    }
+}
---
+++ B3/WPF/GestionEvent/GestionEvent/GestionEvent/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GestionEvent
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+        List<Client> ListClients = new List<Client>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListClients.Add(new Client(NameTextBox.Text, LastNameTextBox.Text, RueTextBox.Text, LocaliteTextBox.Text, PostalTextBox.Text));
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            NomPrenomComboBox.Items.Clear();
+            foreach (Client tmp in ListClients)
+            {
+                NomPrenomComboBox.Items.Add(tmp.GetName() +" " + tmp.GetFirstName());
+            }
+            
+
+        }
+
+        private void NomPrenomComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NomPrenomComboBox.SelectedIndex > -1 && ListClients.Count > 0)
+            MessageBox.Show("Tu as appuyer le nom est :" + ListClients[NomPrenomComboBox.SelectedIndex].GetName());
+            else { }
+        }
+    }
+}
